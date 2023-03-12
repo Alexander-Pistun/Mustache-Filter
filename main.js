e42980d9b181b23eb5a7724f52d726cb8aeb34f0
Alexander-Pistun <110295928+Alexander-Pistun@users.noreply.github.com>
2023-03-12 10:16:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var nose_x=0;
+var nose_y=0;
+
+function preload() {
+    mustache=loadImage('https://i.postimg.cc/6pM6nfbb/mustache.png');
+}
+
+function setup() {  
+    canvas = createCanvas(300, 300);  
+    canvas.center();  
+    video = createCapture(VIDEO);
+    video.size(300, 300);  
+    video.hide();  
+    poseNet = ml5.poseNet(video, modelLoaded);  
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache, nose_x, nose_y, 70, 50);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function modelLoaded() {  
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+        nose_x =results[0].pose.nose.x-35;
+        nose_y =results[0].pose.nose.y;
+    }
+}
